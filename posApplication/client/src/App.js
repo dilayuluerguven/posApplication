adf175bfdb4383a401b992adba4ec6d500b53812
dilayuluerguven <uluerguven245@gmail.com>
2025-02-23 16:11:14 +0300

    Set up the project.

---
+++ posApplication/client/src/App.js
@@ -0,0 +1,12 @@
+import {Button} from "antd"
+
+function App() {
+  return (
+    <div>
+      <Button type="primary">primary button</Button>
+      <h1 className="text-3x1 font bold underline text-red-600">Helloo</h1>
+    </div>
+  );
+}
+
+export default App;
