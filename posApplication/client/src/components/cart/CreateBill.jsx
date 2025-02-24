b69cfdc1b50ce55b1c18e64124ad8bafaac2735b
dilayuluerguven <uluerguven245@gmail.com>
2025-02-24 16:49:25 +0300

    Add PrintBill component and update billing system

--- posApplication/client/src/components/cart/CreateBill.jsx
+++ posApplication/client/src/components/cart/CreateBill.jsx
@@ -1,6 +1,9 @@
-import { Form, Input, Modal, Select } from "antd";
+import { Button, Card, Form, Input, Modal, Select } from "antd";
 
 const CreateBill = ({ isModalOpen, setIsModalOpen }) => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
   return (
     <Modal
       title="Fatura Oluştur"
@@ -8,21 +11,63 @@ const CreateBill = ({ isModalOpen, setIsModalOpen }) => {
       footer={false}
       onCancel={() => setIsModalOpen(false)}
     >
-        <Form layout={"vertical"}>
-            <Form.Item label="Müşteri Adı" rules={[{required:true}]}>               
-                <Input placeholder="Bir Müşteri Adı Yazınız"/>
-            </Form.Item>
-            <Form.Item label="Telefon Numarası" rules={[{required:true}]}>               
-                <Input placeholder="Bir Telefon Numarası Giriniz"/>
-            </Form.Item>
-            <Form.Item label="Ödeme Yöntemi" rules={[{required:true}]}>               
-                <Select placeholder="Bir Ödeme Yöntemi Seçiniz">
-                    <Select.Option value="Nakit">Nakit</Select.Option>
-                    <Select.Option value="Kredi Kartı">Kredi Kartı</Select.Option>
+      <Form layout={"vertical"} onFinish={onFinish}>
+        <Form.Item
+          label="Müşteri Adı"
+          name={"customerName"}
+          rules={[
+            { required: true, message: "Müşteri Adı Alanı Boş Geçilemez" },
+          ]}
+        >
+          <Input placeholder="Bir Müşteri Adı Yazınız"/>
+        </Form.Item>
+        <Form.Item
+          label="Telefon Numarası"
+          name={"phoneNumber"}
+          rules={[
+            { required: true, message: "Telefon Numarası Alanı Boş Geçilemez" },
+          ]}
+        >
+          <Input placeholder="Bir Telefon Numarası Giriniz"  maxLength={11} />
+        </Form.Item>
+        <Form.Item
+          label="Ödeme Yöntemi"
+          name={"paymentMethod"}
+          rules={[
+            { required: true, message: "Ödeme Yöntemi Alanı Boş Geçilemez" },
+          ]}
+        >
+          <Select placeholder="Bir Ödeme Yöntemi Seçiniz">
+            <Select.Option value="Nakit">Nakit</Select.Option>
+            <Select.Option value="Kredi Kartı">Kredi Kartı</Select.Option>
+          </Select>
+        </Form.Item>
 
-                </Select>
-            </Form.Item>
-        </Form>
+        <Card>
+          <div className="flex justify-between">
+            <span>Ara Toplam</span>
+            <span>549.00₺</span>
+          </div>
+          <div className="flex justify-between my-2">
+            <span>KDV Toplam %8</span>
+            <span className="text-red-600">+43.92₺</span>
+          </div>
+          <div className="flex justify-between">
+            <b>Toplam</b>
+            <b>592.92₺</b>
+          </div>
+          <div className="flex justify-end">
+            <Button
+              onClick={() => setIsModalOpen(true)}
+              className="mt-4"
+              type="primary"
+              htmlType="Submit"
+            >
+              Sipariş Oluştur
+            </Button>
+          </div>
+        </Card>
+      </Form>
     </Modal>
   );
 };
--- posApplication/client/src/components/header/Header.jsx
+++ posApplication/client/src/components/header/Header.jsx
@@ -1,15 +1,15 @@
 import { Badge, Input } from 'antd';
 import { SearchOutlined,HomeOutlined,ShoppingCartOutlined,CopyOutlined,UserOutlined,BarChartOutlined,LogoutOutlined } from '@ant-design/icons';
-
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <div className='border-b mb-6'>
       <header className='py-4 px-6 flex justify-between items-center gap-10'>
         <div className="logo">
-          <a href='/'>
+          <Link to='/'>
             <h2 className='text-2xl font-bold md:text-4xl '>LOGO</h2>
-          </a>
+          </Link>
         </div>
         <div className="header-search flex-1 flex justify-center">
           <Input 
@@ -19,44 +19,44 @@ export const Header = () => {
             className='rounded-full max-w-[800px]'/>
         </div>
         <div className="menu-links flex justify-between items-center gap-7 md:static fixed z-50 bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0 border- md:px-0 px-4 py-1">
-          <a href={"/"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
+          <Link to={"/"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
             <HomeOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Ana Sayfa</span>
-          </a>
+          </Link>
           <Badge count={5} offset={[0,6]} className='md:flex hidden'>
-            <a 
-              href={"/"} 
+            <Link 
+              to={"/cart"} 
               className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all"
             >
               <ShoppingCartOutlined  className="md:text-2xl text-xl" />
               <span className="md:text-xs text-[10px]">Sepet</span>
-            </a>
+            </Link>
           </Badge>
-          <a href={"/"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
+          <Link to={"/bills"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
             <CopyOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Faturalar</span>
-          </a>
-          <a href={"/"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
+          </Link>
+          <Link to={"/"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
             <UserOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Müşteriler</span>
-          </a>
-          <a href={"/"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
+          </Link>
+          <Link to={"/"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
             <BarChartOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">İstatistikler</span>
-          </a>
-          <a href={"/"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
+          </Link>
+          <Link to={"/"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
             <LogoutOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Çıkış</span>
-          </a>
+          </Link>
         </div>
         <Badge count={5} offset={[0,6]} className='md:hidden flex'>
-            <a 
-              href={"/"} 
+            <Link 
+              to={"/"} 
               className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all"
             >
               <ShoppingCartOutlined  className="md:text-2xl" />
               <span className="md:text-xs text-[10px]">Sepet</span>
-            </a>
+            </Link>
           </Badge>
       </header>
     </div>
