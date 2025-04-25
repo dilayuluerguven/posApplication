557dc025a61b7d0b13ded81cd244fc19b6931714
dilayuluerguven <uluerguven245@gmail.com>
2025-04-25 17:39:02 +0300

    Add bcryptjs for password hashing and authentication, auth route added

--- posApplication/api/server.js
+++ posApplication/api/server.js
@@ -1,38 +1,37 @@
-const express =require("express");
-const mongoose=require("mongoose");
-const dotenv=require("dotenv");
-const app=express();
-const cors=require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const cors = require("cors");
 
-const port=5000;
+const port = 5000;
 
 //routes
 
-const categoryRoute=require("./routes/categories.js");
-const productRoute=require("./routes/products.js")
-const billRoute=require("./routes/bills.js")
-
-
+const categoryRoute = require("./routes/categories.js");
+const productRoute = require("./routes/products.js");
+const billRoute = require("./routes/bills.js");
+const authRoute = require("./routes/auth.js");
 
 dotenv.config();
 
-const connect =async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("connected to mongoDb");
-    } catch (error) {
-        throw error
-    }
-}
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected to mongoDb");
+  } catch (error) {
+    throw error;
+  }
+};
 //middlewares
 app.use(express.json());
 app.use(cors());
-app.use("/api/categories",categoryRoute);
-app.use("/api/products",productRoute);
-app.use("/api/bills",billRoute);
+app.use("/api/categories", categoryRoute);
+app.use("/api/products", productRoute);
+app.use("/api/bills", billRoute);
+app.use("/api/auth", authRoute);
 
-
-app.listen(port,()=>{
-    connect();
-    console.log(`Listening on port ${port}`);
+app.listen(port, () => {
+  connect();
+  console.log(`Listening on port ${port}`);
 });
