f2e7acb9306cdaaa2f07158f27cd416416cf8f49
dilayuluerguven <uluerguven245@gmail.com>
2025-04-25 16:45:04 +0300

    Categories CRUD operations added.

--- posApplication/api/routes/categories.js
+++ posApplication/api/routes/categories.js
@@ -1,15 +1,44 @@
-const Category =require("../models/Category.js");
+const Category = require("../models/Category.js");
 const express = require("express");
-const router=express.Router();
+const router = express.Router();
 
- router.post("/add-category",async(req,res)=>{
+router.get("/get-all", async (req, res) => {
+  try {
+    const categories = await Category.find();
+    //res.send(categories);
+    res.status(200).json(categories);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/add-category", async (req, res) => {
+  try {
+    const newCategory = new Category(req.body);
+    await newCategory.save();
+    res.status(200).json("Item added.");
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.put("/update-category", async (req, res) => {
+  try {
+    await Category.findOneAndUpdate({ _id: req.body.categoryId }, req.body);
+    res.status(200).json("Item updated.");
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.delete("/delete-category/:id", async (req, res) => {
     try {
-        const newCategory=new Category(req.body);
-        await newCategory.save();
-        res.status(200).json("Item added successfully")
+      await Category.findOneAndDelete({ _id: req.params.id });
+      res.status(200).json("Item deleted.");
     } catch (error) {
-        res.status(400).json(error)
+      console.log(error);
+      res.status(500).json("Bir hata oluştu");
     }
- });
+  });
+  
 
- module.exports=router;
+module.exports = router;
